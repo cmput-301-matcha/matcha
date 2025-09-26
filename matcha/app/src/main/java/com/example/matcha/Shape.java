7159b21193e4fe519e596a2062df00fc71dcadec
Aditya Vaidya <avaidya1@ualberta.ca>
2025-09-25 19:04:30 -0600

    I am adi, I will change Mahogany to Cyan

--- matcha/app/src/main/java/com/example/matcha/Shape.java
+++ matcha/app/src/main/java/com/example/matcha/Shape.java
@@ -3,5 +3,5 @@ package com.example.matcha;
 public abstract class Shape {
     int x;
     int y;
-    String color = "Mahogany";
+    String color = "Cyan"; //I am adi, I will change Mahogany to Cyan
 }
